baba56bb8fc83468a8a7201ae0fcb6a39f0a2ebd
unknown <ajimiyaayoobkhan@gmail.com>
2023-06-28 19:20:33 +0530

    clayays

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Antlr.Runtime.Tree;
+using NEW_CRUD_MVC.DataAccess;
+using NEW_CRUD_MVC.Models;
+
+namespace NEW_CRUD_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDAL _productDAL=new ProductDAL();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var productList=_productDAL.GetAllProducts();
+            if(productList.Count==0)
+            {
+                TempData["InfoMessage"] = "Currently Products are not available in the db";
+            }
+            return View(productList);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var product = _productDAL.GetProductById(id);
+
+                if (product == null)
+                {
+                    TempData["InfoMessage"] = "Product not avilable with Id" + id.ToString();
+                    return RedirectToAction("Index");
+                }
+
+                return View(product);
+            }
+            catch (Exception ex)
+            {
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            bool IsInserted=false;
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    IsInserted = _productDAL.InsertProduct(product);
+
+                    if (IsInserted)
+                    {
+                        TempData["SuccessMessage"] = "Product details saved successfully....";
+
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Unable to save the product details!...";
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var product = _productDAL.GetProductById(id).FirstOrDefault();
+
+            if(product==null)
+            {
+                TempData["InfoMessage"] = "Product not avilable with Id" + id.ToString();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        // POST: Product/Edit/5
+        [HttpPost , ActionName("Edit")]
+        public ActionResult UpdateProduct(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    bool IsUpdate = _productDAL.UpdateProduct(product);
+
+                    if (IsUpdate)
+                    {
+                        TempData["SuccessMessage"] = "Product details Updated successfully....";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Unable to Update the product details!...";
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            catch (Exception ex)
+            {
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }    
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var product = _productDAL.GetProductById(id).FirstOrDefault();
+
+                    if (product==null)
+                    {
+                        TempData["InfoMessage"] = "Product details Deleted successfully....";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Unable to delete  the product details!...";
+                        return RedirectToAction("Index");
+                    }
+                  
+                }
+               return RedirectToAction("Index");
+
+            }
+           
+
+            catch (Exception ex)
+            {
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost , ActionName("Delete")]
+        public ActionResult DeleteConfirmation(int id )
+        {
+
+            try
+            {
+                string result = _productDAL.DeleteProduct(id);
+
+                if (result.Contains("deleted"))
+                {
+                    TempData["SuccessMessage"] = result;
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = result;
+                }
+
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+
+        }
+    }
+}
---
+++ DataAccess/ProductDAL.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using NEW_CRUD_MVC.Models;
+using System.Collections.ObjectModel;
+using System.Web.UI.WebControls;
+
+namespace NEW_CRUD_MVC.DataAccess
+{
+    public class ProductDAL
+    {
+        string conString = ConfigurationManager.ConnectionStrings["addConnectionString"].ToString();
+        private SqlConnection conn;
+
+        //Get ALL PRODUCTS
+        public List<Product> GetAllProducts()
+        { 
+            List<Product> productList = new List<Product>();
+            using (SqlConnection conn = new SqlConnection(conString))
+            {
+                SqlCommand cmd=conn.CreateCommand();
+                cmd.CommandText = "sp_GetAllProducts";
+                SqlDataAdapter sqlDA = new SqlDataAdapter(cmd);
+                DataTable dtProducts = new DataTable();
+
+
+                conn.Open();
+                sqlDA.Fill(dtProducts);
+                conn.Close();
+
+
+                foreach(DataRow dr  in dtProducts.Rows)
+                {
+                    productList.Add(new Product
+                    {
+                        ProductId = Convert.ToInt32( dr["ProductID"]),
+                        ProductName = dr["ProductName"].ToString(),
+                        Price = Convert.ToDecimal( dr["Price"]),
+                        Qty = (int)Convert.ToDecimal(dr["Qty"]),
+                        Remarks = dr["Remarks"].ToString()
+                    });
+                }   
+            }
+
+            return productList;
+        }
+
+
+        //INSERT PRODUCTS
+
+        public bool InsertProduct(Product product)
+        {
+            int id = 0;
+            using (SqlConnection conn = new SqlConnection(conString))
+
+            {
+                SqlCommand cmd = new SqlCommand("sp_InsertProducts", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                cmd.Parameters.AddWithValue("@Price", product.ProductId);
+                cmd.Parameters.AddWithValue("@Qty", product.Qty);
+                cmd.Parameters.AddWithValue("@Remarks", product.Remarks);
+
+
+                conn.Open();
+                id=cmd.ExecuteNonQuery();
+                conn.Close();
+
+            }
+            if(id>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Product By Id
+
+
+        public List<Product> GetProductById(int ProductId)
+        {
+            List<Product> productList = new List<Product>();
+            using (SqlConnection conn = new SqlConnection(conString))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "sp_expGetProductbyId";
+                SqlParameter sqlParameter = cmd.Parameters.AddWithValue("@ProductId",ProductId );
+                SqlDataAdapter sqlDA = new SqlDataAdapter(cmd);
+                DataTable dtProducts = new DataTable();
+
+
+                conn.Open();
+                sqlDA.Fill(dtProducts);
+                conn.Close();
+
+
+                foreach (DataRow dr in dtProducts.Rows)
+                {
+                    productList.Add(new Product
+                    {
+                        ProductId = Convert.ToInt32(dr["ProductId"]),
+                        ProductName = dr["ProductName"].ToString(),
+                        Price = Convert.ToDecimal(dr["Price"]),
+                        Qty = (int)Convert.ToDecimal(dr["Qty"]),
+                        Remarks = dr["Remarks"].ToString()
+                    });
+                }
+            }
+
+            return productList;
+        }
+
+
+        //Update Product
+          public bool UpdateProduct(Product product)
+        {
+            int id = 0;
+            using (SqlConnection conn = new SqlConnection(conString))
+
+            {
+                SqlCommand cmd = new SqlCommand("sp_UpdateProducts", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ProductId", product.ProductId);
+                cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                cmd.Parameters.AddWithValue("@Price", product.ProductId);
+                cmd.Parameters.AddWithValue("@Qty", product.Qty);
+                cmd.Parameters.AddWithValue("@Remarks", product.Remarks);
+
+
+                conn.Open();
+                id=cmd.ExecuteNonQuery();
+                conn.Close();
+
+            }
+            if(id>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Delete Product
+
+        public string DeleteProduct(int ProductId)
+        {
+            string result = "";
+            using (SqlConnection connection=new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand("DeleteValue", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue(" @ProductId ", ProductId);
+                command.Parameters.Add("@Outputmessage", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                result = command.Parameters["@Outputmessage"].Value.ToString();
+                connection.Close();
+
+            }
+
+            return result;
+        }
+
+
+    }
+}
